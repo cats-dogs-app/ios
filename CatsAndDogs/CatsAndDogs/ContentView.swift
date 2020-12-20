12ce5134bea5dad0243dfedef699630d884b7a0a
Halil-Ibrahim-Kasapoglu <36034222+Halil-Ibrahim-Kasapoglu@users.noreply.github.com>
2020-12-20 13:46:37 +0300

    Add simple login screen

--- CatsAndDogs/CatsAndDogs/ContentView.swift
+++ CatsAndDogs/CatsAndDogs/ContentView.swift
@@ -7,10 +7,78 @@
 //
 
 import SwiftUI
+import Firebase
+
+var mainColor: Color = Color.green
 
 struct ContentView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    @State var isLoading = false
+    @State var isSuccessful = false
+    @State var showAlert = false
+    @State var alertMessage = "Well Done"
+    @State var activeUser: String = " none "
+    func login(){
+        self.isLoading = true
+        print("hold on")
+        Auth.auth().signIn(withEmail: username, password: password) { (result, error) in
+             self.isLoading = false
+            print("somehow wdone")
+                 if error != nil {
+                 self.alertMessage = error?.localizedDescription ?? ""
+                    self.showAlert = true
+                 } else {
+                 self.isSuccessful = true
+                    self.activeUser = self.username
+                         DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                                self.isSuccessful = false
+                         }
+                print("well done")
+             }
+          }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+
+        VStack(alignment: .center) {
+            
+            Text("active user : " + self.activeUser).foregroundColor(mainColor)
+            
+            VStack(alignment: .leading){
+                Text("E Mail").font(.headline).foregroundColor(mainColor)
+                TextField("write your email", text: $username).background(mainColor).autocapitalization(.none)
+            }.padding(16)
+
+            
+            VStack(alignment: .leading){
+                Text("Password").font(.headline).foregroundColor(mainColor)
+                TextField("write your password", text: $password).background(mainColor).autocapitalization(.none)
+                        
+            }.padding(16)
+            
+            Button(action: {
+                print("login button clicked")
+                self.login()
+                
+            }, label:{
+                HStack(){
+                    Text("LOGIN").foregroundColor(Color.white)
+                              .padding()
+                        .font(.headline)
+                }.frame(minWidth:0 ,maxWidth: .infinity)
+                .background(mainColor)
+                .cornerRadius(8.0).padding(16)
+            }).alert(isPresented: $showAlert) {
+                     Alert(title: Text("Login Failed"), message: Text("Wear sunscreen"), dismissButton: .default(Text("Got it!")))
+                 }
+        }
+        
+            
+        
+        
     }
 }
 
